99f8c0ac9fbaa379aac7555937f704bc14ac8192
DenisTyubeev <den66683@gmail.com>
2023-09-06 22:01:00 +0200

    lessons  20 myfirstservlet

---
+++ lessons20/src/main/java/org/example/MyFirstServlet.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/home")
+public class MyFirstServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println(
+                "<html>" +
+                        "<body>" +
+                        "<h1>My first servlet</h1>" +
+                        "</body" +
+                        "</html"
+        );
+
+        resp.addCookie(new Cookie("key", "value"));
+
+    }
+}
