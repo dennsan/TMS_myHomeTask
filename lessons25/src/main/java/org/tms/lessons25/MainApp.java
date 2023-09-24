8233c75752409d3489012040727fc5ad668e8799
DenisTyubeev <den66683@gmail.com>
2023-09-24 19:26:43 +0200

    lessons 25 singleton

---
+++ lessons25/src/main/java/org/tms/lessons25/MainApp.java
@@ -0,0 +1,9 @@
+package org.tms.lessons25;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Action action = Action.getInstance("Move");
+        Action action1 = Action.getInstance("Jump");
+        Action action2 = Action.getInstance("Run");
+    }
+}
