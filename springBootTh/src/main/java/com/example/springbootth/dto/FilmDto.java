9fe4dd8599bcddcea4473216cf216732391143aa
Denis_Tyubeev <den66683@gmail.com>
2024-01-13 00:39:56 +0300

    lesson 45 validation film

--- springBootTh/src/main/java/com/example/springbootth/dto/FilmDto.java
+++ springBootTh/src/main/java/com/example/springbootth/dto/FilmDto.java
@@ -1,14 +1,15 @@
 package com.example.springbootth.dto;
 
 import com.example.springbootth.enumerated.Genre;
+import com.example.springbootth.validation.CheckTitleNameValidation;
 import lombok.Data;
 
 @Data
-
+@CheckTitleNameValidation
 public class FilmDto {
-private Integer id;
-private String title;
-private Integer age;
-private Genre genre;
-private Double rating;
+    private Integer id;
+    private String title;
+    private Integer age;
+    private Genre genre;
+    private Double rating;
 }
--- springBootTh/src/main/java/com/example/springbootth/service/impl/FilmServiceImpl.java
+++ springBootTh/src/main/java/com/example/springbootth/service/impl/FilmServiceImpl.java
@@ -1,18 +1,22 @@
 package com.example.springbootth.service.impl;
 
+import com.example.springbootth.dto.FilmDto;
 import com.example.springbootth.entity.FilmEntity;
+import com.example.springbootth.mapper.FilmMapper;
 import com.example.springbootth.repository.FilmRepository;
 import com.example.springbootth.service.FilmService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class FilmServiceImpl implements FilmService {
     private final FilmRepository repository;
+    private final FilmMapper mapper;
 
     @Override
     @Transactional
@@ -24,5 +28,18 @@ public class FilmServiceImpl implements FilmService {
             FilmEntity filmEntity = film.get();
             filmEntity.setRating(rating);
         }
+    }
+
+    @Override
+    public List<FilmDto> getAll() {
+        var all = repository.findAll();
+        return mapper.toDtos(all);
+    }
+
+    @Override
+    public FilmDto save(FilmDto dto) {
+        var entity = mapper.toEntity(dto);
+        FilmEntity saved = repository.save(entity);
+        return mapper.toDto(saved);
     }
 }
