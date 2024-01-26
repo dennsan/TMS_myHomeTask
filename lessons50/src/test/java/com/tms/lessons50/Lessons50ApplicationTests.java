bf1c8f6710b2452e3e4d1ddcd7ddb29c86eafad5
Denis_Tyubeev <den66683@gmail.com>
2024-01-26 21:37:19 +0300

    lessons 50 test

---
+++ lessons50/src/test/java/com/tms/lessons50/Lessons50ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.lessons50;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lessons50ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
